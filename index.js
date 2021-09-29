5a8911b3501d655c641831c4f401af7ec5804390
Gurupriyan <gurupriyan98@gmail.com>
2021-09-29 14:24:20 +0000

    Solved the problems in index.js

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 //1
 function countToTen(){
-  
+  for (let i = 1; i <= 10; i++){
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne( num ){
+    for (let i = 1; i < num; i++){
+        console.log(i);
+    }
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+    for (let i = 1; i <= num; i++){
+        if(i % 2 === 0){
+            console.log(i);
+        }
+    }
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for (let i= 1; i <= num; i += 2){
+      console.log(i);
+  }
 }
 
 //5 
-function countEvens(){
-  
+let evenArray = [];
+function countEvens(array){
+  for (let i = 0; i < array.length; i++){
+      if (array[i] % 2 === 0){
+          evenArray.push(array[i]);
+      }
+  }
+  return evenArray;
 }
 
+
+countToTen();
+countFromOne(12);
+countEveryEven(14);
+countEveryOdd(15);
+console.log(countEvens([1,2,3,4,5,6,7,8,9,10]));
